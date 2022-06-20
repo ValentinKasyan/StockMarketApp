8d06f0ace2cecf896eae8f7148540091b44740b6
Valentin <valentinKasyan@gmail.com>
2022-06-20 16:11:10 +0300

    stock info

--- app/src/main/java/com/inter/stockmarketapp/presentation/company_info/CompanyInfoScreen.kt
+++ app/src/main/java/com/inter/stockmarketapp/presentation/company_info/CompanyInfoScreen.kt
@@ -1,7 +1,19 @@
 package com.inter.stockmarketapp.presentation.company_info
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.inter.stockmarketapp.ui.theme.DarkBlue
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Composable
@@ -11,6 +23,74 @@ fun CompanyInfoScree(
     viewModel: CompanyInfoViewModel = hiltViewModel()
 ) {
     val state = viewModel.state
+    if (state.error == null) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(DarkBlue)
+                .padding(16.dp)
+        ) {
+            state.company?.let { company ->
+                Text(
+                    text = company.name,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = company.symbol,
+                    fontStyle = FontStyle.Italic,
+                    fontSize = 14.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider(
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Industry: ${company.industry}",
+                    fontSize = 14.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Country: ${company.country}",
+                    fontSize = 14.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider(
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = company.description,
+                    fontSize = 12.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                //if intraday data available
+                if (state.stockInfos.isNotEmpty()) {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Market Summary"
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    StockChart(
+                        infos = state.stockInfos,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(250.dp)
+                            .align(CenterHorizontally)
+                    )
+                }
+            }
+        }
+    }
 
 
 }
